9d16bbf63f68aa4b02cbf996795f904f717fbec8
CLEMENT-ALOYSIUS-2347219 <141571339+CLEMENT-ALOYSIUS-2347219@users.noreply.github.com>
2024-08-21 14:41:00 +0530

    update

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,9 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
+
 import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+
 import gradglide from "../../Assets/Projects/gradglide.png";
 import TermTalk from "../../Assets/Projects/termtalk.png";
 import survey from "../../Assets/Projects/survey.png";
